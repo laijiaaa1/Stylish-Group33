35a9f4778d08ca24df977d6f5c8f29563f6608e1
Red Wang <redxoxored@gmail.com>
2023-11-04 15:22:23 +0800

    Reinstall pods

---
+++ iOS/STYLiSH/Managers/APIProviders/CollectionProvider.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionProvider.swift
+//  STYLiSH
+//
+//  Created by Red Wang on 2023/11/4.
+//  Copyright © 2023 AppWorks School. All rights reserved.
+//
+
+import UIKit
+
+class CollectionProvider {
+    
+    
+    
+
+}
